99264e80f78a005a48c6c4513e7375da3a0bd41f
Yeasin <ar9485727@gmail.com>
2024-08-16 22:29:45 +0600

    live link added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173","https://job-task2-client.vercel.app"],
   credentials: true,
 };
 
